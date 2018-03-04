aaa12c384bbe0f211e885ef2b370dedfd2251753
Elian Kurtenkov <elian90@abv.bg>
2018-03-04 13:51:00 +0200

    Campaigns contract

---
+++ Contracts/contracts/Campaigns.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.18;
+// We have to specify what version of compiler this code will compile with
+
+contract Campaigns {
+  
+
+      struct Campaign {
+            bool isActive;
+            string hashHex;
+      }
+
+      mapping(uint8 => Campaign) public campaigns;
+  
+      function Campaigns() public {
+      }
+
+      function getCampaignHash(uint8 id) view public returns (string) {
+            return campaigns[id].hashHex;
+      }
+
+      function addCampaign(uint8 id, bool isActive, string campaignHash) public {
+            campaigns[id] = Campaign(isActive, campaignHash);
+      }
+
+      function closeCampaign(uint8 id) public {
+          campaigns[id].isActive = false;
+      }
+}
--- Contracts/migrations/2_deploy_contracts.js
+++ Contracts/migrations/2_deploy_contracts.js
@@ -3,3 +3,7 @@ var Voting = artifacts.require("./Voting.sol");
 module.exports = function(deployer) {
   deployer.deploy(() => Voting([1,2,3,4]));
 };
+
+module.exports = function(deployer) {
+  deployer.deploy(Campaigns);
+};
