80ebef811f021c3c843d404161c781e2f0957ae9
Elian Kurtenkov <elian90@abv.bg>
2018-03-04 19:53:58 +0200

    Participation contract base code

--- Contracts/contracts/CampaignParticipation.sol
+++ Contracts/contracts/CampaignParticipation.sol
@@ -2,9 +2,23 @@ pragma solidity ^0.4.18;
 
 contract CampaignParticipation {
 
-      mapping(address => uint) public participation;
+      struct Participant {
+            address addr;
+            uint8 donatedAmount;
+      }
+
+      address _donationAddress;
+      uint8 _campaignId;
+      uint8 _minFundingAmount;
+      uint8 _commissionPercent;
+      uint8 _foundersAddress;
+
+      Participant[] public participants;
   
-      function CampaignParticipation() public {
+      function CampaignParticipation(address donationAddress, uint8 campaignId, uint8 minFundingAmount) public {
+            _donationAddress = donationAddress;
+            _campaignId = campaignId;
+            _minFundingAmount = minFundingAmount;
       }
 
       function getCollectedAmount() view public returns (uint8) {
@@ -18,5 +32,32 @@ contract CampaignParticipation {
       function receiveFunds() public {
             //this contract should be able to receive tokens
             // and store the info about who sent what
+      }
+
+      function triggerExecution() public {
+            if (getCollectedAmount() >= _minFundingAmount)
+            {
+                  participateInEvent();
+            } else {
+                  returnMoneyToSenders();
+            }
+      }
+
+      function participateInEvent() private {
+            uint8 total = getCollectedAmount();
+            uint8 commission = total * _commissionPercent;
+            makeTransaction(_donationAddress, total - commission);
+            makeTransaction(_foundersAddress, commission);
+            //if anything fails, revert
+      }
+
+      function returnMoneyToSenders() private {
+            for (uint8 i = 0; i < participants.length; i++) {
+                  makeTransaction(participants[i].addr, participants[i].donatedAmount);
+            }
+      }
+
+      function makeTransaction(address addr, uint8 amount) private {
+
       }
 }
