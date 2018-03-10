bb5d78273def701749c23256114272c9045974e9
Kristian Smilenov <kristian.smilenov@gmail.com>
2018-03-10 20:36:58 +0200

    List view updates after new contract is deployed

--- Web/src/web_app/js/admin.js
+++ Web/src/web_app/js/admin.js
@@ -2,7 +2,7 @@
 
   forceMetamask();
   initWeb3();
-  
+
   var app = new Vue({
     el: '#wallet-app',
     created: function () {
@@ -12,83 +12,106 @@
     data: {
       views: ["campaignsList", "createCampaign", "tokensList", "createTokens"],
       currentView: "campaignsList",
+
+      // User when creating new crowdfunding campaign
       crowdfundingContract_beneficiaryAddress: '',
       crowdfundingContract_endDate: '1522576800',
       crowdfundingContract_conversionRate: 1000,
       crowdfundingContract_description: 'Pre-sale #1',
       crowdfundingContract_minCap: 1000,
-      
+
+      // Used when creating new token
       tokenContract_name: 'Crypto Investoment Fund',
       tokenContract_symbol: 'CIF',
       tokenContract_totalSupply: '1000',
       tokenContract_rate: '1000',
       tokenContract_minInvestment: '1000',
-      
+
+      // Show user token balance for selected contract
       selectedTokenContractSymbol: '',
       selectedTokenContractAddress: '',
-
       tokenContractUserAddress: '',
-      tokenContractUserAddressBalance: '',
 
       userAddress: '',
-      sendToAddress: '',
-      sendValueAmount: '',
+      //sendToAddress: '',
+      //sendValueAmount: '',
       savedCampaigns: [],
       savedTokens: []
     },
     methods: {
-      setActiveTokenContract: function(token) {
+      changeView: function (viewName) {
+        this.currentView = viewName;
+        switch (viewName) {
+          case "campaignsList":
+            this.fetchCampaignsFromDB();
+            break;
+          case "tokensList":
+            this.fetchTokensFromDB();
+            break;
+        }
+      },
+
+      setActiveTokenContract: function (token) {
         this.selectedTokenContractSymbol = token.symbol;
         this.selectedTokenContractAddress = token.address;
       },
-      fetchContractsFromDB: function () {
-        var self = this;
-        getCampaignContractsFromDB()
-        .then((campaigns) => {
-            self.savedCampaigns = campaigns;
-        })
-        .catch(err => console.log(err));
 
-        getTokenContractsFromDB().then((tokens) => {
-            self.savedTokens = tokens;
-        })
-        .catch(err => console.log(err));  
+      fetchContractsFromDB: function () {
+        this.fetchCampaignsFromDB();
+        this.fetchTokensFromDB();
       },
 
-      invalidateCampaign: async function(campaignAddress) {
+      fetchCampaignsFromDB() {
+        var self = this;
+        getCampaignContractsFromDB()
+          .then((campaigns) => {
+            self.savedCampaigns = campaigns;
+          })
+          .catch(err => console.log(err));
+      },
+
+      fetchTokensFromDB() {
+        var self = this;
+        getTokenContractsFromDB().then((tokens) => {
+          self.savedTokens = tokens;
+        })
+          .catch(err => console.log(err));
+      },
+
+      invalidateCampaign: async function (campaignAddress) {
         var userAddress = await getMetaMaskAccount();
         var fundraiserContract = await getContractByAddress('CampaignTokenFundraiser', campaignAddress);
 
         fundraiserContract.methods.invalidate().send(
-            { from: userAddress, gas: gas, gasPrice: gasPrice }, function (error, result) {
-              if(error) showError(error);
-              else showSuccess(result);
-            });
+          { from: userAddress, gas: gas, gasPrice: gasPrice }, function (error, result) {
+            if (error) showError(error);
+            else showSuccess(result);
+          });
       },
 
-      closeCampaign: async function(campaignAddress) {
+      closeCampaign: async function (campaignAddress) {
         var userAddress = await getMetaMaskAccount();
         var fundraiserContract = await getContractByAddress('CampaignTokenFundraiser', campaignAddress);
 
         fundraiserContract.methods.close().send(
-            { from: userAddress, gas: gas, gasPrice: gasPrice }, function (error, result) {
-              if(error) showError(error);
-              else showSuccess(result);
-            });
+          { from: userAddress, gas: gas, gasPrice: gasPrice }, function (error, result) {
+            if (error) showError(error);
+            else showSuccess(result);
+          });
       },
 
-      finalizeCampaign: async function(campaignAddress) {
+      finalizeCampaign: async function (campaignAddress) {
         var userAddress = await getMetaMaskAccount();
         var fundraiserContract = await getContractByAddress('CampaignTokenFundraiser', campaignAddress);
 
         fundraiserContract.methods.sendTokens().send(
-            { from: userAddress, gas: gas, gasPrice: gasPrice }, function (error, result) {
-              if(error) showError(error);
-              else showSuccess(result);
-            });
+          { from: userAddress, gas: gas, gasPrice: gasPrice }, function (error, result) {
+            if (error) showError(error);
+            else showSuccess(result);
+          });
       },
 
-      refreshGrid: function() {
+      refreshGrid: function () {
         var b = this.savedCampaigns;
         this.savedCampaigns = [];
         this.savedCampaigns = b;
@@ -100,42 +123,42 @@
         await this.getCampaignParticipantsCount(campaignAddress);
       },
 
-      getCampaignState: async function(campaignAddress) {
+      getCampaignState: async function (campaignAddress) {
         var state = await getCampaignState(campaignAddress);
         this.savedCampaigns
           .find(c => c.fundraiserContractAddress == campaignAddress)
           .state = state;
 
-          this.refreshGrid();
+        this.refreshGrid();
       },
 
-      getCampaignBalance: async function(campaignAddress) {
+      getCampaignBalance: async function (campaignAddress) {
         var bal = await getCampaignBalance(campaignAddress);
 
         this.savedCampaigns
           .find(c => c.fundraiserContractAddress == campaignAddress)
           .balance = bal;
 
-          this.refreshGrid();
+        this.refreshGrid();
       },
 
-      contributeToCampaign: async function(campaignAddress) {
+      contributeToCampaign: async function (campaignAddress) {
         await contributeToCampaign(campaignAddress, await getMetaMaskAccount(), 0.01);
       },
 
-      getCampaignParticipantsCount: async function(campaignAddress) {
+      getCampaignParticipantsCount: async function (campaignAddress) {
         var count = await getCampaignParticipantsCount(campaignAddress);
         this.savedCampaigns
           .find(c => c.fundraiserContractAddress == campaignAddress)
           .participantCount = count;
 
-          this.refreshGrid();
+        this.refreshGrid();
       },
 
-      convertToETH: function(wei) {
+      convertToETH: function (wei) {
         return web3.utils.fromWei(wei.toString(), 'ether');
       },
-      
+
       deployContract: deployContract,
       deployCrowdfundingContract: deployCrowdfundingContract,
       deployTokenContract: deployTokenContract,
